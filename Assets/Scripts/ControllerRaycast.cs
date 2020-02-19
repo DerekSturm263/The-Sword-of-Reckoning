8cfca8b6df000c87bbe1ba33d48c108b80168aea
Derek Sturm <dereksturm263@gmail.com>
2020-02-19 13:34:07 -0800

    a

---
+++ Assets/Scripts/ControllerRaycast.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerRaycast : MonoBehaviour
+{
+    public LineRenderer lr;
+    public Vector3 pointEnd;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, Vector3.forward * 2);
+    }
+}
